48ec1e1739fe8755c29810d2b5ed68a193f1f67f
lukeengle11 <luke.engle@hotmail.com>
2015-12-02 12:50:33 -0800

    beautification

--- search.php
+++ search.php
@@ -6,53 +6,64 @@
 	<link rel="stylesheet" type="text/css" href="nintendo.css" />
 </head>
 <body>
-<img src="img/nintendo_logo_trans.gif" alt="nintendo logo"/>
-<p>
-	<?php
-		require('Parsing.class.php');
-		$base_url = "http://nintendo.wikia.com/wiki/";
-		$final_url;
-		$game_title;
+	<div id="nav">
+		<ul>
+			<li><a href="main.html">Home Page</a></li>
+			<li><a href="top_100.php">Top 100 Nintendo Games</a></li>
+			<li><a href="nes.html">Search NES Games</a></li>
+			<li><a href="snes.html">Search SNES Games</a></li>
+			<li><a href="n64.html">Search N64 Games</a></li>
+		</ul>
+	</div>
+	<img class="padding" src="img/nintendo_logo_trans.gif" alt="nintendo logo"/>
+	<div class="search">
+		<p>
+			<?php
+				require('Parsing.class.php');
+				$base_url = "http://nintendo.wikia.com/wiki/";
+				$final_url;
+				$game_title;
 
-		if(count($_POST) > 0)
-		{
-			$parser = new Parsing;
-			$game_title = str_replace(" ", "_", $_POST['title']);
-			
-			if($game_title)
-				$final_url = $base_url . $game_title;
-			else
-				echo "Something went wrong.";
+				if(count($_POST) > 0)
+				{
+					$parser = new Parsing;
+					$game_title = str_replace(" ", "_", $_POST['title']);
+					
+					if($game_title)
+						$final_url = $base_url . $game_title;
+					else
+						echo "Something went wrong.";
 
-			try {
-				$web_contents = file_get_contents($final_url);
-				$parsed_contents = $parser->get_string_between($web_contents, "<p><i><b>", "</p>\n");
-				if (empty($parsed_contents))
-				{
-					$parsed_contents = $parser->get_string_between($web_contents, "</table><i><b>", "</p>\n");
-				}
-				
-				$final_contents = strip_tags($parsed_contents, '<p></p>');
-				
-				if (empty($final_contents))
-				{
-					echo "Search returned 0 results";
+					try {
+						$web_contents = file_get_contents($final_url);
+						$parsed_contents = $parser->get_string_between($web_contents, "<p><i><b>", "</p>\n");
+						if (empty($parsed_contents))
+						{
+							$parsed_contents = $parser->get_string_between($web_contents, "</table><i><b>", "</p>\n");
+						}
+						
+						$final_contents = strip_tags($parsed_contents, '<p></p>');
+						
+						if (empty($final_contents))
+						{
+							echo "Search returned 0 results";
+						}
+						else
+						{
+							echo "<h2>" . $_POST['title'] . "</h2>";
+							print $final_contents;	
+						}
+					} catch (Exception $e) {
+						print $e;
+					}
+
 				}
 				else
 				{
-					echo "<h1>" . $_POST['title'] . "</h1>";
-					print $final_contents;	
 				}
-			} catch (Exception $e) {
-				print $e;
-			}
-
-		}
-		else
-		{
-		}
-	?>
-</p>
-<a href="search_db.html">Back to Search</a>
+			?>
+		</p>
+		<a href="main.html">Back to Home</a>
+	</div>
 </body>
 </html>
--- top_100.php
+++ top_100.php
@@ -5,18 +5,30 @@
 	<link rel="stylesheet" type="text/css" href="nintendo.css"/>
 </head>
 <body>
-<a href="main.html"><img src="img/nintendo_logo_trans.gif" alt="nintendo logo" /></a>
-<h1>Top 100 Nintendo Games</h1>
-<ol>
-<?php
-	require('Parsing.class.php');
+	<div id="nav">
+			<ul>
+				<li><a href="main.html">Home Page</a></li>
+				<li><a href="top_100.php">Top 100 Nintendo Games</a></li>
+				<li><a href="nes.html">Search NES Games</a></li>
+				<li><a href="snes.html">Search SNES Games</a></li>
+				<li><a href="n64.html">Search N64 Games</a></li>
+			</ul>
+		</div>
+	<img class="padding" src="img/nintendo_logo_trans.gif" alt="nintendo logo" />
+	<div class="search">
+		<h2>Top 100 Nintendo Games</h2>
+		<ol>
+		<?php
+			require('Parsing.class.php');
 
-	$parser = new Parsing;
-	$url = "http://nintendo.wikia.com/wiki/Nintendo_Power's_100_Best_Games_of_All_Time";
-	$web_contents = file_get_contents($url);
+			$parser = new Parsing;
+			$url = "http://nintendo.wikia.com/wiki/Nintendo_Power's_100_Best_Games_of_All_Time";
+			$web_contents = file_get_contents($url);
 
-	$parsed_contents = $parser->get_string_between($web_contents, "<ol>", "<script>");
-	print $parsed_contents;
-?>
+			$parsed_contents = $parser->get_string_between($web_contents, "<ol>", "<script>");
+			print $parsed_contents;
+		?>
+		</ol>
+	</div>
 </body>
 </html>
